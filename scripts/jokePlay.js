5ed098f247e7d3b906a5265f8dbddfeda7431ec7
温昊然 <wenhaoran0110@163.com>
2019-11-12 11:09:03 +0800

    main

--- scripts/jokePlay.js
+++ scripts/jokePlay.js
@@ -8,13 +8,41 @@ document.getElementById('cn').onclick = () => {
   document.querySelector('.randomize').textContent = '随机生成笑话';
 };
 
-document.getElementById('us').onclick =
+document.getElementById('us').onclick = () =>{
+	document.title = 'sSilly story generator';
+	document.getElementById('customname').textContent = 'Enter custom name:';
+	document.getElementById('lbl-cn').textContent = 'CN';
+  document.getElementById('lbl-us').textContent = 'US';
+  document.getElementById('lbl-uk').textContent = 'UK';
+  document.getElementById('customname').placeholder = 'Bob';
+  document.querySelector('.randomize').textContent = 'Generate random story';
+}
 document.getElementById('uk').onclick = () => {
   document.title = 'Silly story generator';
   document.getElementById('lbl-customname').textContent = 'Enter custom name:';
   document.getElementById('lbl-cn').textContent = 'CN';
   document.getElementById('lbl-us').textContent = 'US';
   document.getElementById('lbl-uk').textContent = 'UK';
-  document.getElementById('customname').placeholder = 'Bob';
+  document.getElementById('customname').placeholder = 'Harry';
   document.querySelector('.randomize').textContent = 'Generate random story';
 };
+
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+function randomValueFromArray(array){
+	const random = Math.floor(Math.random()*array.length);
+	return array[random];
+}
+const storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+const insertX = ['Willy the Goblin','Big Daddy','Father Christmas'];
+const insertY = ['the soup kitchen','Disneyland','the White House'];
+const insertZ = ['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
+
+randomize.addEventListener('click',result);
+function result(){
+	let newStory,xItem,yItem,zItem,name;
+	if(document.getElementById('cn'.checked)){
+		
+	}
+}
